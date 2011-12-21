f9709a5e4ef850b776d3ce7425d72a2acb16ff23
Aaron Swartz <me@aaronsw.com>
2011-12-21 16:30:22 -0500

    add safestr

--- sendmail.py
+++ sendmail.py
@@ -1,5 +1,25 @@
 import os
-from web import safestr, webapi
+from web import webapi
+
+def safestr(obj, encoding='utf-8'):
+    r"""
+    Converts any given object to utf-8 encoded string. 
+    
+        >>> safestr('hello')
+        'hello'
+        >>> safestr(u'\u1234')
+        '\xe1\x88\xb4'
+        >>> safestr(2)
+        '2'
+    """
+    if isinstance(obj, unicode):
+        return obj.encode(encoding)
+    elif isinstance(obj, str):
+        return obj
+    elif hasattr(obj, 'next'): # iterator
+        return itertools.imap(safestr, obj)
+    else:
+        return str(obj)
 
 def sendmail(from_address, to_address, subject, message, headers=None, **kw):
     """

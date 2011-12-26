8b75428be47a365fd7300c5ca9085addba051020
Aaron Swartz <me@aaronsw.com>
2011-12-26 17:55:18 -0500

    sigh

--- app.py
+++ app.py
@@ -567,8 +567,7 @@ class disp:
     GET = POST = lambda self, path: dispatch(path)
 
 if __name__ == "__main__":
-    web.sendmail = sendmail
-    web.webapi.internalerror = web.emailerrors('me@aaronsw.com', web.webapi.internalerror)
+    #web.webapi.internalerror = web.emailerrors('me@aaronsw.com', web.webapi.internalerror)
 
     web.config.db_parameters = dict(dbn='postgres', user='u6oil71cps8c9p', pw='pspl9af55q0hdb020j6q0c64kn', db='ds0sc5fk8iv8g', host='ec2-107-21-107-108.compute-1.amazonaws.com', maxcached=20, maxconnections=80, blocking=True)
     web.config.db_printing = True

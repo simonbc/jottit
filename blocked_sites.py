6b8ec752ad82a9e2490e22568661f886535ecf5f
Sean B. Palmer <sean@miscoranda.com>
2015-01-30 16:04:42 +0000

    Added another site to the blocked sites list

--- blocked_sites.py
+++ blocked_sites.py
@@ -1,1 +1,1 @@
-blocked_sites = ['absa1.jottit.com', 'standardbank.jottit.com', 'mastercard-co-za-.jottit.com', 'update.jottit.com', 'bankofamerica.jottit.com', 'standardbankcoza.jottit.com', 'bankofamerica-com.jottit.com']
+blocked_sites = ['absa1.jottit.com', 'standardbank.jottit.com', 'mastercard-co-za-.jottit.com', 'update.jottit.com', 'bankofamerica.jottit.com', 'standardbankcoza.jottit.com', 'bankofamerica-com.jottit.com', 'kqaskdi.jottit.com']
